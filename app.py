c68306f46c4644d597933dc18ddd7a557520dcd2
Vinícius Dias Sant'Ana <161242816+Vin-icius@users.noreply.github.com>
2025-09-10 19:48:23 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from datetime import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    server_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    client_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+    server_ip = socket.gethostbyname(socket.gethostname())
+
+    return {
+        "server_time": server_time,
+        "client_ip": client_ip,
+        "server_ip": server_ip
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
